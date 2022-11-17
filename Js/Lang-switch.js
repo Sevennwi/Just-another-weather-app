a38098ab53034d4ccc18bb918a367ccb3d99ab7f
Killian <brunetkillian@yahoo.fr>
2022-11-17 18:37:04 +0800

    Lang correction

--- Js/Lang-switch.js
+++ Js/Lang-switch.js
@@ -1,8 +1,8 @@
 let userLang = navigator.language || navigator.userLanguage;
-if (!sessionStorage.getItem("fr") && userLang.includes("fr")) {
-  sessionStorage.setItem("fr", "Yes");
-  window.location.replace("/Lang/fr.html");
-} else if (!sessionStorage.getItem("en")) {
+if (!sessionStorage.getItem("en") && userLang.includes("en")) {
   sessionStorage.setItem("en", "Yes");
   window.location.replace("../index.html");
+} else if (!sessionStorage.getItem("fr") && userLang.includes("fr")) {
+  sessionStorage.setItem("fr", "Yes");
+  window.location.replace("/Lang/fr.html");
 }
